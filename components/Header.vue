29792aa48c986c11d6d775ab25a59c4c262c6713
cocoa-1012 <cocoa.pandaok@gmail.com>
2021-10-28 10:32:45 -0400

    init

---
+++ components/Header.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="flex items-center flex-col justify-center">
+    <div
+      class="
+        container
+        relative
+        flex
+        mt-4
+        justify-between
+        sm:items-center sm:pt-0
+        max-w-screen-xl
+      "
+    >
+      <div class="flex">
+        <NuxtLink to="/" class="mr-3">
+          <img src="images/location.png" loading="lazy" alt="" class="logo" />
+        </NuxtLink>
+        <p class="text-yellow-600 font-bold leading-4">
+          723 Broad Street <br />
+          Lake Charles, LA 70601
+        </p>
+      </div>
+      <div class="text-yellow-600 font-bold">
+        <Nuxt-link to="/testimonials" class="m-5">Testimonials</Nuxt-link>
+        <Nuxt-link to="/videos" class="m-5">Videos</Nuxt-link>
+        <Nuxt-link to="/scholarship" class="m-5">Scholarship</Nuxt-link>
+      </div>
+      <NuxtLink to="/" class="mr-3">
+        <img src="images/search.png" loading="lazy" alt="" class="logo" />
+      </NuxtLink>
+    </div>
+    <div
+      class="
+        container
+        relative
+        flex
+        justify-between
+        sm:items-start sm:pt-12
+        max-w-screen-xl
+      "
+    >
+      <NuxtLink to="/" class="mr-3">
+        <img src="images/logo-desktop.png" loading="lazy" alt="" class="logo" />
+      </NuxtLink>
+      <div class="text-white font-bold">
+        <Nuxt-link to="/home" class="m-5">Home</Nuxt-link>
+        <Nuxt-link to="/our-firm" class="m-5">Our firm</Nuxt-link>
+        <Nuxt-link to="/practice-areas" class="m-5">Practice Areas</Nuxt-link>
+        <Nuxt-link to="/blog" class="m-5">Blog</Nuxt-link>
+        <Nuxt-link to="/contact-us" class="m-5">Contact Us</Nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // data() {
+  //   return {
+  //     isOpen: false,
+  //     burgerOpen: false,
+  //   };
+  // },
+  // methods: {
+  //   toggle() {
+  //     this.isOpen = !this.isOpen;
+  //     document.getElementById("myDropdown").classList.toggle("show");
+  //     document.getElementById("myDropdown").classList.add("w--open");
+  //   },
+  //   burgerToggle() {
+  //     this.burgerOpen = !this.burgerOpen;
+  //     document.getElementById("myDropdown").classList.toggle("show");
+  //     document.getElementById("myDropdown").classList.add("w--open");
+  //   },
+  // },
+};
+</script> 
---
+++ components/HomeMain.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <div
+      class="
+        flex
+        h-screen
+        bg-gray-100 bg-main-texture bg-center bg-no-repeat bg-cover
+        -mt-28
+        items-center
+        justify-center
+      "
+    >
+      <p class="mt-96 font-bold text-5xl text-center text-white">
+        COMMITTED TO SOUTHWEST LOUISIANA. <br />COMMITTED TO HOME.
+      </p>
+    </div>
+    <div class="container bg-gray-200"></div>
+  </div>
+
+  <!-- <div
+      class="
+        relative
+        flex
+        items-top
+        justify-around
+        bg-gray-100
+        sm:items-center sm:pt-0
+      "
+    >
+      <div class="flex">
+        <NuxtLink to="/" class="mr-3">
+          <img src="images/location.png" loading="lazy" alt="" class="logo" />
+        </NuxtLink>
+        <p class="text-yellow-600 font-bold leading-4">
+          723 Broad Street <br />
+          Lake Charles, LA 70601
+        </p>
+      </div>
+      <div class="text-yellow-600 font-bold">
+        <Nuxt-link to="/att-index" class="m-5">Testimonials</Nuxt-link>
+        <Nuxt-link to="/att-index" class="m-5">Videos</Nuxt-link>
+        <Nuxt-link to="/att-index" class="m-5">Scholarship</Nuxt-link>
+      </div>
+      <NuxtLink to="/" class="mr-3">
+        <img src="images/search.png" loading="lazy" alt="" class="logo" />
+      </NuxtLink>
+    </div>
+    <div
+      class="relative flex justify-around bg-gray-100 sm:items-start sm:pt-12"
+    >
+      <NuxtLink to="/" class="mr-3">
+        <img src="images/logo-desktop.png" loading="lazy" alt="" class="logo" />
+      </NuxtLink>
+      <div class="text-white font-bold">
+        <Nuxt-link to="/att-index" class="m-5">Home</Nuxt-link>
+        <Nuxt-link to="/att-index" class="m-5">Our firm</Nuxt-link>
+        <Nuxt-link to="/att-index" class="m-5">Practice Areas</Nuxt-link>
+        <Nuxt-link to="/att-index" class="m-5">Blog</Nuxt-link>
+        <Nuxt-link to="/att-index" class="m-5">Contact Us</Nuxt-link>
+      </div>
+    </div> -->
+</template>
+
+<script>
+export default {
+  // data() {
+  //   return {
+  //     isOpen: false,
+  //     burgerOpen: false,
+  //   };
+  // },
+  // methods: {
+  //   toggle() {
+  //     this.isOpen = !this.isOpen;
+  //     document.getElementById("myDropdown").classList.toggle("show");
+  //     document.getElementById("myDropdown").classList.add("w--open");
+  //   },
+  //   burgerToggle() {
+  //     this.burgerOpen = !this.burgerOpen;
+  //     document.getElementById("myDropdown").classList.toggle("show");
+  //     document.getElementById("myDropdown").classList.add("w--open");
+  //   },
+  // },
+};
+</script> 
