8c66163f4d9546e281d196af772a22ac291c2c14
cocoa-1012 <cocoa.pandaok@gmail.com>
2021-10-28 11:32:11 -0400

    home page update

--- components/HomeMain.vue
+++ components/HomeMain.vue
@@ -14,61 +14,79 @@
         COMMITTED TO SOUTHWEST LOUISIANA. <br />COMMITTED TO HOME.
       </p>
     </div>
-    <div class="container bg-gray-200"></div>
-  </div>
-
-  <!-- <div
+    <div
       class="
-        relative
-        flex
-        items-top
-        justify-around
-        bg-gray-100
-        sm:items-center sm:pt-0
+        flex flex-col
+        bg-gray-200
+        items-center
+        justfy-center
+        font-bold
+        text-gray-800
       "
     >
-      <div class="flex">
-        <NuxtLink to="/" class="mr-3">
-          <img src="images/location.png" loading="lazy" alt="" class="logo" />
-        </NuxtLink>
-        <p class="text-yellow-600 font-bold leading-4">
-          723 Broad Street <br />
-          Lake Charles, LA 70601
+      <p class="text-2xl mb-8 -mt-4">Real Case Results</p>
+      <div class="flex max-w-screen-xl justify-around w-3/5">
+        <div
+          v-for="(data, index) in cardData"
+          :key="index"
+          class="flex flex-col items-left mb-24"
+        >
+          <p class="text-3xl">{{ data.price }}</p>
+          <div class="bg-yellow-600 w-16 h-2 m-3 ml-6"></div>
+          <p class="text-2xl">{{ data.title }}</p>
+        </div>
+      </div>
+    </div>
+    <div class="flex grid grid-cols-2 bg-white">
+      <div>
+        <img src="images/Image 3@1X.png" alt="" class="w-full" />
+      </div>
+
+      <div class="flex flex-col justify-around m-12">
+        <p class="text-green-600 font-bold text-4xl mb-4">
+          <span class="border-b-8 border-yellow-600">Why</span> Hire Us?
+        </p>
+        <p class="text-3xl font-semibold ml-12 w-4/5">
+          Our team of skilled lawyers really is a team. We collaborate freely
+          with one another to provide the best strategies for clients.
+        </p>
+        <p class="text-2xl ml-12 mt-4 w-4/5">
+          Each of our Lake Charles attorneys today continues this sense of
+          community and continuity. We apply our combined 135 years of
+          experience to achieve great success for our clients. We invite you to
+          look deeper into each attorney’s bios.
         </p>
       </div>
-      <div class="text-yellow-600 font-bold">
-        <Nuxt-link to="/att-index" class="m-5">Testimonials</Nuxt-link>
-        <Nuxt-link to="/att-index" class="m-5">Videos</Nuxt-link>
-        <Nuxt-link to="/att-index" class="m-5">Scholarship</Nuxt-link>
-      </div>
-      <NuxtLink to="/" class="mr-3">
-        <img src="images/search.png" loading="lazy" alt="" class="logo" />
-      </NuxtLink>
     </div>
-    <div
-      class="relative flex justify-around bg-gray-100 sm:items-start sm:pt-12"
-    >
-      <NuxtLink to="/" class="mr-3">
-        <img src="images/logo-desktop.png" loading="lazy" alt="" class="logo" />
-      </NuxtLink>
-      <div class="text-white font-bold">
-        <Nuxt-link to="/att-index" class="m-5">Home</Nuxt-link>
-        <Nuxt-link to="/att-index" class="m-5">Our firm</Nuxt-link>
-        <Nuxt-link to="/att-index" class="m-5">Practice Areas</Nuxt-link>
-        <Nuxt-link to="/att-index" class="m-5">Blog</Nuxt-link>
-        <Nuxt-link to="/att-index" class="m-5">Contact Us</Nuxt-link>
-      </div>
-    </div> -->
+  </div>
 </template>
 
 <script>
 export default {
-  // data() {
-  //   return {
-  //     isOpen: false,
-  //     burgerOpen: false,
-  //   };
-  // },
+  data() {
+    return {
+      isOpen: false,
+      burgerOpen: false,
+      cardData: [
+        {
+          price: "$262 M.",
+          title: "Class Judgment",
+        },
+        {
+          price: "$36 M.",
+          title: "Train Accident",
+        },
+        {
+          price: "$17.5 M.",
+          title: "Class Settlement",
+        },
+        {
+          price: "$10.8 M.",
+          title: "Truck Accident",
+        },
+      ],
+    };
+  },
   // methods: {
   //   toggle() {
   //     this.isOpen = !this.isOpen;
